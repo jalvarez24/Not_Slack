7673171afd3cbc1ef7ac5b1c3cb387760a233107
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2020-08-13 19:45:51 -0700

    Updated Header comp and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { Avatar } from '@material-ui/core'
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import SearchIcon from '@material-ui/icons/Search';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import Tooltip from '@material-ui/core/Tooltip';
 
 interface Props {
     text: string;
@@ -12,19 +13,23 @@ interface Props {
 const Header: React.FC<Props> = ({ text }) => {
     return <div className='header'>
         <div className='header__left'>
+            <Tooltip title={<span style={{fontSize: '18px'}}>History</span>} className='iconContainer' arrow>
+                <AccessTimeIcon />
+            </Tooltip>
+        </div>
+        <div className='header__middle'>
+            <SearchIcon />
+            <input placeholder='Search Awesome Team'></input>
+        </div>
+        <div className="header__right">
+            <Tooltip title={<span style={{fontSize: '18px'}}>Help</span>} className='iconContainer' arrow>
+                <HelpOutlineIcon />
+            </Tooltip>
             <Avatar
                 className='head__avatar'
                 alt='Jayro Alvarez'
                 src=''
             />
-            <AccessTimeIcon />
-        </div>
-        <div className='header__middle'>
-            <SearchIcon />
-            <input placeholder='Search Awesome Team' ></input>
-        </div>
-        <div className="header__right">
-            <HelpOutlineIcon />
         </div>
     </div>;
   }
