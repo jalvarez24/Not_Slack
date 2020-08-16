f830eaf433ca1caa77138ec7063f46334ab1ce72
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2020-08-15 22:38:50 -0700

    Checks for google auth, signin

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,35 @@ import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Chat from './components/Chat'
+import Login from './components/Login'
+import { useStateValue } from './components/StateProvider'
 import './App.css'
 
 const App: React.FC = () => {
+
+  const [{ user }] = useStateValue()
+
   return <div className='App'>
     <Router>
-      <Header />
-      <div className='app__body'>
-        <Sidebar />
+      {!user ? 
+      <Login />
+      :
+      <>
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
 
-        <Switch>
-          <Route path='/room/:roomId'>
-            <Chat />
-          </Route>
-          <Route path='/'>
-            <h1>Welcome</h1>
-          </Route>
-        </Switch>
-      </div>
+          <Switch>
+            <Route path='/room/:roomId'>
+              <Chat />
+            </Route>
+            <Route path='/'>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </>
+      }
     </Router>
   </div>;
 }
